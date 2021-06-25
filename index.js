ef1698a39ab0c8c931807d0d08c73a9c9e762bde
Shruti <78341076+DevSkugal@users.noreply.github.com>
2021-06-25 22:21:19 +0530

    ost

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express(); app.use(cors());
 const request = require('request');
+var bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 
 var mongoDataBase;
@@ -16,6 +17,12 @@ client.connect(function(err) {
 
   client.close();
 });
+
+app.use(cors());
+app.options('*', cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "512mb" }));
+app.use(bodyParser.urlencoded({ limit: "512mb", extended: true, parameterLimit: 1000000 }));
 // MongoClient.connect(url, function (err, db) {
 //     if (!err) {
 //         mongoDataBase = db.db("Book-Store");
@@ -29,6 +36,18 @@ app.listen(9000, () => {
     console.log('Server listinng on 9000')
 });
 
-app.post('/update',(req,res)=> {
-    
+app.post('/insert',(req,res)=> {
+  console.log("req",req.body.name)
+  res.set('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') {
+      res.set('Access-Control-Allow-Methods', 'POST');
+      res.set('Access-Control-Allow-Headers', 'Content-Type');
+      res.set('Access-Control-Max-Age', '3600');
+      res.status(204).send('');
+  } else {
+    var data = req.body;
+    console.log(data);
+    res.send("ok")
+   // db.books.insert(data);
+  }
 })
